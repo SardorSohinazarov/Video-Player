09016c2a538734bcd1bf3f545ef6f14f78e59c27
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-08-17 04:55:08 +0500

    Chotki

--- Video Player/Form1.Designer.cs
+++ Video Player/Form1.Designer.cs
@@ -29,35 +29,102 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ochishToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.playlistToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.axWindowsMediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer)).BeginInit();
             this.SuspendLayout();
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem,
+            this.playlistToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1039, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ochishToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // ochishToolStripMenuItem
+            // 
+            this.ochishToolStripMenuItem.Name = "ochishToolStripMenuItem";
+            this.ochishToolStripMenuItem.Size = new System.Drawing.Size(136, 26);
+            this.ochishToolStripMenuItem.Text = "Ochish";
+            this.ochishToolStripMenuItem.Click += new System.EventHandler(this.ochishToolStripMenuItem_Click);
+            // 
+            // playlistToolStripMenuItem
+            // 
+            this.playlistToolStripMenuItem.Name = "playlistToolStripMenuItem";
+            this.playlistToolStripMenuItem.Size = new System.Drawing.Size(69, 24);
+            this.playlistToolStripMenuItem.Text = "Playlist";
+            this.playlistToolStripMenuItem.Click += new System.EventHandler(this.playlistToolStripMenuItem_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Black;
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.listBox1.ForeColor = System.Drawing.Color.White;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(802, 28);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(237, 552);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            this.listBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.listBox1_KeyUp);
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
             // axWindowsMediaPlayer
             // 
+            this.axWindowsMediaPlayer.Dock = System.Windows.Forms.DockStyle.Fill;
             this.axWindowsMediaPlayer.Enabled = true;
-            this.axWindowsMediaPlayer.Location = new System.Drawing.Point(-2, 0);
+            this.axWindowsMediaPlayer.Location = new System.Drawing.Point(0, 28);
             this.axWindowsMediaPlayer.Name = "axWindowsMediaPlayer";
             this.axWindowsMediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer.OcxState")));
-            this.axWindowsMediaPlayer.Size = new System.Drawing.Size(640, 360);
-            this.axWindowsMediaPlayer.TabIndex = 0;
-            this.axWindowsMediaPlayer.Enter += new System.EventHandler(this.axWindowsMediaPlayer_Enter);
+            this.axWindowsMediaPlayer.Size = new System.Drawing.Size(802, 552);
+            this.axWindowsMediaPlayer.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(851, 441);
+            this.ClientSize = new System.Drawing.Size(1039, 580);
             this.Controls.Add(this.axWindowsMediaPlayer);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "S Video Player";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ochishToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ToolStripMenuItem playlistToolStripMenuItem;
         private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer;
     }
 }
--- Video Player/Form1.cs
+++ Video Player/Form1.cs
@@ -1,12 +1,6 @@
-﻿using AxWMPLib;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using System;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace Video_Player
@@ -18,6 +12,8 @@ namespace Video_Player
         public Form1()
         {
             InitializeComponent();
+
+            listBox1.Visible = false;
 
             this.Resize += Form1_Resize;
             formOriginalSize = this.Size;
@@ -35,11 +31,11 @@ namespace Video_Player
 
         private void axWindowsMediaPlayer_Enter(object sender, EventArgs e)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
+            /*OpenFileDialog openFileDialog = new OpenFileDialog();
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
                 axWindowsMediaPlayer.URL = openFileDialog.FileName;
-            }
+            }*/
         }
 
         void Form1_Resize(object sender, EventArgs e)
@@ -59,6 +55,76 @@ namespace Video_Player
             c.Location = new Point(newX, newY);
             c.Size = new Size(newWidth, newHeight);
 
+        }
+
+        private void ochishToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Visible = true;
+
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.ShowDialog();
+            string rootPath = dialog.SelectedPath;
+            string[] pathes = Directory.GetFiles(rootPath);
+
+            foreach (string path in pathes)
+            {
+                listBox1.Items.Add(path);
+            }
+        }
+
+        private void mediaPlayFromPath(string path)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                axWindowsMediaPlayer.URL = openFileDialog.FileName;
+            }
+        }
+
+        private void mediaPlay()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                axWindowsMediaPlayer.URL = openFileDialog.FileName;
+            }
+        }
+        
+        private void selectedPlay()
+        {
+            var selectedMediaPath = listBox1.GetItemText(listBox1.SelectedItem);
+            axWindowsMediaPlayer.URL = selectedMediaPath;
+            axWindowsMediaPlayer.Ctlcontrols.play();
+        }
+
+        private async void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            listBox1.Visible = false;
+
+            var selectedMediaPath = listBox1.GetItemText(listBox1.SelectedItem);
+            axWindowsMediaPlayer.URL = selectedMediaPath;
+            axWindowsMediaPlayer.Ctlcontrols.play();
+        }
+
+        private void listBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            listBox1.Visible = false;
+
+            if (e.KeyCode == Keys.Enter) 
+            { 
+                selectedPlay();
+            }
+        }
+
+        private void playlistToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Visible = true;
         }
     }
 }
